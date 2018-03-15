ef0fc93339790fc3fec1a26648fddb54e2aedc46
Felipe Santos <felipe@gtrconsultoria.com.br>
2018-03-15 13:27:31 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+
+
+<template>
+  <div class="corpo">
+
+    <meu-menu :rotas="routes"/>
+
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+
+  </div>
+</template>
+
+<script>
+
+import { routes }  from './routes';
+import Menu from './components/shared/menu/Menu.vue';
+
+export default {
+
+  components: {
+    'meu-menu': Menu
+  },
+
+  data() {
+
+    return {
+
+      routes
+    }
+  }
+};
+</script>
+
+<style>
+.corpo {
+  font-family: Arial, Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .3s
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
+  }
+</style>
